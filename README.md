91cc05ce90b282b28dd1daabe14df66a55bfde6f
ahensel823 <ahensel823@gmail.com>
2022-08-05 19:48:28 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-rcarmh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-rcarmh)
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './main.css';
+import Expenses from './routes/expenses';
+import Invoices from './routes/invoices';
+
+let rootElement = document.getElementById('root');
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
+  </BrowserRouter>
+);
